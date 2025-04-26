1a7661859514d42ca3988385f5722648217c7788
M. Iqbal Effendi <iqbaleff214@gmail.com>
2025-04-26 13:55:05 +0800

    01:10:06 - 01:50:42

--- src/main.rs
+++ src/main.rs
@@ -149,3 +149,108 @@ fn augmented_assignment_test() {
     a %= 3;
     println!("variable a value is {}", a);
 }
+
+#[test]
+fn boolean_test() {
+    let a = true;
+    let b: bool = false;
+    
+    println!("{} is true.", a);
+    println!("{} is false.", b);
+}
+
+#[test]
+fn comparison_test() {
+    let a = 10;
+    let b = 20;
+    
+    let result:bool = a > b;
+    println!("{} > {} is {}", a, b, result);
+}
+
+#[test]
+fn logical_test() {
+    let a = true;
+    let b = false;
+    
+    println!("{} and {} is {}", a, b, a && b);
+    println!("{} or {} is {}", a, b, a || b);
+    println!("not {} is {}", a, !a);
+}
+
+#[test]
+fn character_test() {
+    let a = 'a';
+    let b: char = 'b';
+    
+    println!("{} is a char.", a);
+    println!("{} is a char.", b);
+}
+
+#[test]
+fn tuple_test() {
+    let data = (10, 10.5, 'a', true);
+    println!("{:?}", data);
+    
+    let data: (i32, f64, char, bool) = (10, 10.5, 'a', true);
+    println!("{:?}", data);
+    println!("{:?}", data.3);
+    
+    let (a, _, c, d) = data;
+    println!("{} {} {}", a, c, d);
+    
+    let mut data = (10, 10.5, 'a', true);
+    println!("{:?}", data);
+    data.3 = false;
+    println!("{:?}", data);
+}
+
+fn unit() {
+    println!("Hello, world!");
+}
+
+#[test]
+fn unit_test() {
+    let result: () = unit();
+    println!("{:?}", result);
+}
+
+#[test]
+fn array_test() {
+    let data: [i32; 5] = [10, 20, 30, 40, 50];
+    println!("{:?}", data);
+    
+    println!("{:?}", data[0]);
+    println!("{:?}", data[1]);
+    println!("{:?}", data[2]);
+    println!("{:?}", data[3]);
+    println!("{:?}", data[4]);
+    
+    let mut data = [10, 20, 30, 40, 50];
+    println!("{:?}", data);
+    data[0] = 100;
+    println!("{:?}", data);
+    
+    println!("Size of array is {}", data.len());
+}
+
+#[test]
+fn two_dimensional_array_test() {
+    let data: [[i32; 5]; 3] = [
+        [10, 20, 30, 40, 50], 
+        [100, 200, 300, 400, 500], 
+        [1000, 2000, 3000, 4000, 5000]
+    ];
+    
+    println!("{:?}", data);
+    println!("{:?}", data[0][0]);
+}
+
+const MAXIMUM: i32 = 100;
+
+#[test]
+fn constant_test() {
+    const MINIMUM: i32 = 10;
+    println!("Maximum value is {}.", MAXIMUM);
+    println!("Minimum value is {}.", MINIMUM);
+}
